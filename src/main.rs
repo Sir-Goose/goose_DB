7d3bb9f50b0bef04ece235c884ae67a50affbfb9
Matthew Allison <matthewallisonxyz@gmail.com>
2023-10-05 21:26:34 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,17 @@ use clap::*;
 
 #[derive(Parser)]
 struct Arguments {
-    read : String,
-    write : String,
-    delete : String
+    arg_1 : String,
+    arg_2 : String,
+    arg_3 : String
 }
 
 fn main() {
+    let arguments : Arguments = Arguments::parse();
 
+    println!("Argument 1: {} Argument 2: {} Argument 3: {}", arguments.arg_1, arguments.arg_2, arguments.arg_3)
 
-
-
-
-
-    }
+}
     fn decision_tree() {
 
     }
