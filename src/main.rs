c6e47313d2ae84e0533ef9f89ea244c4815d07e7
Matthew Allison <matthewallisonxyz@gmail.com>
2023-10-07 09:52:49 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,15 @@ fn main() {
     let arguments : Arguments = Arguments::parse();
 
     println!("Argument 1: {} Argument 2: {} Argument 3: {}", arguments.arg_1, arguments.arg_2, arguments.arg_3)
+    decision_tree(arguments.arg_1)
 
 }
-fn decision_tree(database_operation : String) {
-    let read : String = "Read"
-
+fn decision_tree(database_operation : String)  {
+    let mut read = String::new();
+    read.push_str("Read");
 
     match database_operation {
-        "Read" => {
+        read => {
             return "read"
         }
         _ => println!("{} is not a valid operation.", database_operation),
