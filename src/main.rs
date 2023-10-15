f008f70ed4d7f35ef7129d59c0d886a614cc8315
Matthew Allison <matthewallisonxyz@gmail.com>
2023-10-15 22:34:29 +0200

    Wrote the logic for save to disk function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use clap::*;
 use std::collections::HashMap;
+use std::io::Write;
 use bincode;
+use serde::{Serialize, Deserialize};
+
 
 
 
@@ -14,6 +17,7 @@ struct Arguments {
     arg_3 : String
 }
 
+#[derive(Serialize, Deserialize)]
 struct Database {
     map: HashMap<String, String>,
 }
@@ -72,8 +76,33 @@ fn modify_database(key : String, value : String) {
 fn delete_from_database(key : String, value : String) {
     println!("Deleting data from the database");
 }
-fn save_to_disk(database : Database) {
-    // implement serialisation
+fn save_to_disk(database: &Database) -> Result<(), Box<dyn std::error::Error>> {
+    // Serialize the database to JSON or any other desired format
+    let serialized = serde_json::to_string(database)?;
+
+    // Create a new file.
+    let mut file = std::fs::File::create("database.json")?;
+
+    // Write the serialized data to the file.
+    file.write_all(serialized.as_bytes())?;
+
+    Ok(())
+
+
+}
+/*
+future self use this when calling save_to_disk to handle the error
+
+ if let Err(err) = save_to_disk(&your_database) {
+    eprintln!("Failed to save to disk: {}", err);
+    // Handle the error appropriately (e.g., exit the program or retry).
+}
+
+ */
+
+
+fn load_from_disk(database_name : String) {
+    // implement deserialization
 }
 
 
